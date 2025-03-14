9e5fa775e32b4cacef393a10bd6e3f56bf051d10
kartik0920 <kartik0920@gmail.com>
2025-03-14 21:16:00 +0530

    solving conflict

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,25 +1,23 @@
-
 import GameComponent from "./GameComponent";
 import games from "./GamesData";
 
 export default function Games() {
-  return <>
-  <div className="gamesPage my-20">
-    <h1>GAMES</h1>  
-    {
-      games.map((game)=>(
-        <GameComponent 
-          key={game.id}
-          id={game.id}
-          title={game.title}
-          link={game.link}
-          description={game.description} 
-          randomShapeImage={game.randomShapeImage}
-          image={game.image}
+  return (
+    <>
+      <div className="gamesPage my-20" id="game">
+        <h1>GAMES</h1>
+        {games.map((game) => (
+          <GameComponent
+            key={game.id}
+            id={game.id}
+            title={game.title}
+            link={game.link}
+            description={game.description}
+            randomShapeImage={game.randomShapeImage}
+            image={game.image}
           />
-      ))
-    }  
-   
-  </div>
-  </>
+        ))}
+      </div>
+    </>
+  );
 }
