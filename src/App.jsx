044731dcc086997d59f4b327e568a3d72d4d6a20
kartik0920 <kartik0920@gmail.com>
2025-03-18 23:41:25 +0530

    Adding about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Games from "./components/Games";
 import Timeline from "./components/Timeline";
 import Speakers from "./components/Speaker";
 import Sponsors from "./components/Sposor";
+import About from "./components/About";
+import Temp from "./components/Temp";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
       <Games />
       <Speakers />
       <Timeline />
       <Sponsors />
+      {/* <Temp />? */}
     </>
   );
 }
